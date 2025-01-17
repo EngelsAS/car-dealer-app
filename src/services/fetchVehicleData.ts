8b3ad65dc7adeada76da192cc0ac19aa7d1c8d8e
EngelsAS <engelsantero@gmail.com>
2025-01-16 21:09:09 -0300

    feat: add fetchVehicleData

---
+++ src/services/fetchVehicleData.ts
@@ -0,0 +1,19 @@
+import { VehicleDataType } from "@/types/VehicleDataType";
+import { api } from "../../utils/axiosInstance";
+
+export const fetchVehicleData = async (makeId: string, year: string) => {
+  try {
+    const resp = await api.get(
+      `/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`,
+    );
+
+    console.log(resp.data);
+
+    const data = resp.data.Results as VehicleDataType[];
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
